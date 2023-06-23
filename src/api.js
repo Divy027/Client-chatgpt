1c72a4744b233a60f62e2396af53508c6d504b94
Divy027 <105413340+Divy027@users.noreply.github.com>
2023-06-23 16:57:22 +0530

    Update api.js

--- src/api.js
+++ src/api.js
@@ -1,6 +1,6 @@
 export const fetchResponse =  async(chat) => {
     try {
-        const response = await fetch("https://chatgpt-server-qh7n0cwfg-divy027.vercel.app/", { 
+        const response = await fetch('https://chatgpt-server-silk.vercel.app/', { 
             method: 'POST',
             headers: {
                 "Content-Type": "application/json"
